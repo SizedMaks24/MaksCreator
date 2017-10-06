92e822b82781b473c5c213045e00308d7df516a2
SizedMaks24 <shvint74@gmail.com>
2017-10-06 14:11:24 +0300

    MaxCreat

--- index.php
+++ index.php
@@ -3,11 +3,30 @@ require 'vendor/autoload.php' ;
 echo "Hi,my name is Max";
 $app = new \atk4\ui\App('Добро Пожаловать!') ;
 $app->initLayout('Centered') ;
-$button = $app -> add('Button') ;
+
+$button = $app -> add('Button1') ;
 $button ->set ('Registration') ;
 $button ->icon = 'plus' ;
 $button->link('https://vk.com') ;
-$button = $app -> add('Button') ;
-$button ->set ('Steam Profile') ;
+
+$button = $app -> add('Button2') ;
+$button ->set ('Apple') ;
 $button ->icon = 'laptop' ;
 $button->link('https://www.apple.com') ;
+
+$button = $app -> add('Button3') ;
+$button ->set ('Steam ') ;
+$button ->icon = 'laptop' ;
+$button->link('http://store.steampowered.com') ;
+
+$button1 = $app ->add ('Button') ;
+$button1->set('Registartation') ;
+$button1->addClass('massive green') ;
+
+$button2 = $app ->add ('Button') ;
+$button2->set('Apple') ;
+$button2->addClass('massive yellow') ;
+
+$button3 = $app ->add ('Button') ;
+$button3->set('Steam') ;
+$button3->addClass('huge red') ;
