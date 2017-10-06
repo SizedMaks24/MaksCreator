0314c054d4f225127b70d59bb87be92de2cbb75d
SizedMaks24 <shvint74@gmail.com>
2017-10-06 14:27:15 +0300

    max

--- index.php
+++ index.php
@@ -4,32 +4,22 @@ require 'vendor/autoload.php' ;
 $app = new \atk4\ui\App('Добро Пожаловать!') ;
 $app->initLayout('Centered') ;
 
-$button = $app -> add('Button') ;
-$button ->set ('Registration') ;
-$button ->icon = 'plus' ;
-$button->link('https://vk.com') ;
 
-$button = $app -> add('Button') ;
-$button ->set ('Apple') ;
-$button ->icon = 'laptop' ;
-$button->link('https://www.apple.com') ;
-
-$button = $app -> add('Button') ;
-$button ->set ('Steam ') ;
-$button ->icon = 'laptop' ;
-$button->link('http://store.steampowered.com') ;
 
 $button1 = $app ->add ('Button') ;
 $button1->set('Registartation') ;
 $button1->addClass('massive green') ;
-$button->link('https://vk.com') ;
+$button1->link('https://vk.com') ;
+$button1->icon = 'plus' ;
 
 $button2 = $app ->add ('Button') ;
 $button2->set('Apple') ;
 $button2->addClass('massive yellow') ;
-$button->link('https://www.apple.com') ;
+$button2->link('https://www.apple.com') ;
+$button2->icon = 'laptop'
 
 $button3 = $app ->add ('Button') ;
 $button3->set('Steam') ;
 $button3->addClass('huge red') ;
-$button->link('http://store.steampowered.com') ;
+$button3->link('http://store.steampowered.com') ;
+$button3->icon = 'game'
