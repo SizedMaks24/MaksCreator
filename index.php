feba70848cda1fe760018d75cb0bef6edc3c2e0c
SizedMaks24 <shvint74@gmail.com>
2017-10-06 14:20:24 +0300

    Max

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require 'vendor/autoload.php' ;
-echo "Hi,my name is Max";
+
 $app = new \atk4\ui\App('Добро Пожаловать!') ;
 $app->initLayout('Centered') ;
 
@@ -22,11 +22,14 @@ $button->link('http://store.steampowered.com') ;
 $button1 = $app ->add ('Button') ;
 $button1->set('Registartation') ;
 $button1->addClass('massive green') ;
+$button->link('https://vk.com') ;
 
 $button2 = $app ->add ('Button') ;
 $button2->set('Apple') ;
 $button2->addClass('massive yellow') ;
+$button->link('https://www.apple.com') ;
 
 $button3 = $app ->add ('Button') ;
 $button3->set('Steam') ;
 $button3->addClass('huge red') ;
+$button->link('http://store.steampowered.com') ;
