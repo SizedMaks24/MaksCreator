455fc8088aa18238649b8b205acbac4391c51ab7
SizedMaks24 <shvint74@gmail.com>
2017-10-06 18:25:33 +0300

    Revert "TheMaxCreator4"
    This reverts commit ee3d2b8c8766a54b7af77f866709a059d27ff572.

--- index.php
+++ index.php
@@ -8,24 +8,25 @@ $app->initLayout('Centered') ;
 
 $button1 = $app ->add ('Button') ;
 $button1->set('Registartation') ;
-$button1->addClass('massive red') ;
+$button1->addClass('massive green') ;
 $button1->link('https://vk.com') ;
 $button1->icon = 'plus' ;
 
 $button2 = $app ->add ('Button') ;
 $button2->set('Apple') ;
-$button2->addClass('massive orange') ;
+$button2->addClass('massive yellow') ;
 $button2->link('https://www.apple.com') ;
 $button2->icon = 'laptop' ;
 
 $button3 = $app ->add ('Button') ;
 $button3->set('Steam') ;
-$button3->addClass('massive yellow') ;
+$button3->addClass('huge red') ;
 $button3->link('http://store.steampowered.com') ;
 $button3->icon = 'game' ;
 
-$button4 = $app ->add ('Button') ;
-$button4->set ('1a') ;
-$button->addClass ('huge green') ;
-$button4->link('https://www.1a.lv/')
-$button4->icon = 'store'
+$button4 =$app->add(['button','Google','iconRight'=>'browser']) ;
+
+$bar =$app ->add(['ui'=>'vertical buttons']) ;
+$bar ->add(['button','Play','icon'=>'play']) ;
+$bar ->add(['button','Stop','icon'=>'stop']) ;
+$bar ->add(['button','Download','icon'=>'download']) ;
