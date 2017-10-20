e9c543c6dd4af967ebd6966b8f2b932c64ca8ac1
SizedMaks24 <shvint74@gmail.com>
2017-10-20 14:51:37 +0300

    Max

--- index.php
+++ index.php
@@ -34,29 +34,4 @@ $img = 'http://2.bp.blogspot.com/-HWgpuULTHMc/UZnitEtOKwI/AAAAAAAAAOo/YZcxUkgFGp
 $icon = $app ->add(['Image', $img ]) ;
 
 $text =$app ->add(['Text','Força Atletico Madrid']) ;
-$text->addParagraph('Fan'); 
-=======
-$button4 =$app ->add ('Button') ;
-$button4->set('1a') ;
-$button4->addClass('huge green') ;
-$button4->link ('https://www.1a.lv/') ;
-$button4->icon = 'game' ;
-
-$button5=$app ->add ('Button') ;
-$button5->set('YouTube') ;
-$button5->addClass('huge blue') ;
-$button5->link ('https://www.youtube.com/') ;
-$button5->icon ='plus' ;
-
-$button6=$app ->add ('Button') ;
-$button6->set('heroku') ;
-$button6->addClass ('small dark blue') ;
-$button6->link ('https://heroku.com') ;
-$button6->icon = 'plus' ;
-
-$button7=$app ->add ('Button') ;
-$button7->set('Twitch') ;
-$button7->addClass ('small purple') ;
-$button7->link ('https://twitch.tv/') ;
-$button7->icon = 'plus' ;
->>>>>>> ddd8c957ebc08d90fd3728767de7bdaff1332429
+$text->addParagraph('Fan') ; 
