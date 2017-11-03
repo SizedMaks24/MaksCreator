55ece47a7b838a0b5e7042d9740aca9b1e2bf545
SizedMaks24 <shvint74@gmail.com>
2017-11-03 15:28:24 +0200

    Max

--- Test.php
+++ Test.php
@@ -5,3 +5,5 @@ $app = new \atk4\ui\App('Welocme to Fun Page') ;
 $app->initLayout('Centered') ;
 $c = $_GET['Name'] ;
 $label = $app->add(['Label',$c]) ;
+$c = $_GET ['Surname'] ;
+$label = $app->add(['Label',$c]) ;
