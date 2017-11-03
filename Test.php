26a57ea6af74d8535a92d084dec9fbaea02d31d0
SizedMaks24 <shvint74@gmail.com>
2017-11-03 15:35:29 +0200

    Max

--- Test.php
+++ Test.php
@@ -4,6 +4,5 @@ require 'vendor/autoload.php' ;
 $app = new \atk4\ui\App('Welocme to Fun Page') ;
 $app->initLayout('Centered') ;
 $c = $_GET['Name'] ;
-$label = $app->add(['Label',$c]) ;
-$c = $_GET ['Surname'] ;
-$label = $app->add(['Label',$c]) ;
+$h = $_GET ['Surname'] ;
+$label = $app->add(['Label',$c.' '.$h]) ;
